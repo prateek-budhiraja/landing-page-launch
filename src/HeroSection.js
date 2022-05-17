7ae106abb1b6ac74685917daeae863094f249f4c
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2022-05-17 13:53:15 +0530

    added favicon, modified text

--- src/HeroSection.js
+++ src/HeroSection.js
@@ -6,7 +6,10 @@ const HeroSection = () => {
   return (
     <Row>
       <Col className="p-4 col-12 col-sm-12 col-md-6 col-lg-6">
-        <img className="apple-image" src="./images/apple.jpg" />
+        <img
+          className="apple-image"
+          src={process.env.PUBLIC_URL + "/images/apple.jpg"}
+        />
       </Col>
       <Col className="p-4 col-12 col-sm-12 col-md-6 col-lg-6">
         <h1 className="mb-5">Out of league.</h1>
@@ -14,8 +17,8 @@ const HeroSection = () => {
           I don't even want to think about what life would be like without
           appleeeee. You too?
           <br /> <br />
-          Signup with your email to never get any appleeeee photos to make your
-          day bright! ☀️
+          Signup with your email to never get any appleeeee photos that could
+          make your day bright! ☀️
         </h4>
         <InputGroup className="form mt-5">
           <Input
